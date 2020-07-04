fe49f9c968cbe13233e2712b1a86b32aba8bf078
harshb02 <58955446+harshb02@users.noreply.github.com>
2020-07-05 00:30:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var names =["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function () {
 
-for(var i=0;i<names.length;i++)
-{
-	 var firstLetter = names[i].charAt(0).toLowerCase();
-	 if (firstLetter === 'j') 
-	 {
-    console.log("Good Bye" + " " + names[i]);
-     } 
-     else 
-     {
-    console.log("Hello" +" "+names[i]);
-     }
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
 }
 
+})();
